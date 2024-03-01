3de7742230faa083bab471111813c27ad03f3fc5
keneth217 <kipyegonkeneth03@gmail.com>
2024-03-01 15:15:37 +0300

    classses mapper

---
+++ src/main/java/com/example/exams/entity/Classes.java
@@ -0,0 +1,24 @@
+package com.example.exams.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Classes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String className;
+    private int population;
+}

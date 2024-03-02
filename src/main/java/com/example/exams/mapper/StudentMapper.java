0cd3c34c87ef54e0569d6a64f002116a4903517f
keneth217 <kipyegonkeneth03@gmail.com>
2024-03-02 11:42:23 +0300

    student dto mapper

--- src/main/java/com/example/exams/mapper/StudentMapper.java
+++ src/main/java/com/example/exams/mapper/StudentMapper.java
@@ -1,44 +1,33 @@
 package com.example.exams.mapper;
 
 import com.example.exams.dtos.StudentsDto;
-//import com.example.exams.dtos.SubjectDto;
+
 import com.example.exams.entity.Students;
+import org.modelmapper.ModelMapper;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class StudentMapper {
+
+    private static ModelMapper modelMapper = new ModelMapper();
+
+    public StudentMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
     //map dto to entity
     public static Students mapToStudent(StudentsDto studentsDto){
 
-        Students students = new Students(
-                studentsDto.getId(),
-               studentsDto.getAdmNo(),
-               studentsDto.getStudentName(),
-               studentsDto.getStudentClass(),
-               studentsDto.getGender(),
-               studentsDto.getTermName(),
-               studentsDto.getTermAdmitted(),
-              studentsDto.getPhone()
-//                studentsDto.getSubjects().stream().map(subject1 -> subject1.getSubjectCode()).collect(Collectors.toSet())
-//               studentsDto.getSubjects().stream().map(SubjectDto::new).collect(Collectors.toSet())
-        );
+        Students students = new Students();
+        students=modelMapper.map(studentsDto,Students.class);
         return students;
     }
 
 //map entity to dto
 public static StudentsDto mapToStudentDtos(Students students){
-    StudentsDto studentsDto = new StudentsDto(
-            students.getId(),
-            students.getAdmNo(),
-            students.getStudentName(),
-            students.getStudentClass(),
-            students.getGender(),
-            students.getTermName(),
-            students.getTermAdmitted(),
-            students.getPhone()
-//        students.getSubjects().stream().map()
-    );
+    StudentsDto studentsDto = new StudentsDto();
+    studentsDto=modelMapper.map(students,StudentsDto.class);
     return studentsDto;
 }
 }

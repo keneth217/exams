7ed8b1df5cdd21e77f0bede66e438a1c04da49c4
keneth217 <kipyegonkeneth03@gmail.com>
2024-03-02 11:43:29 +0300

    teacher dto mapper

--- src/main/java/com/example/exams/mapper/TeacherMapper.java
+++ src/main/java/com/example/exams/mapper/TeacherMapper.java
@@ -3,31 +3,26 @@ package com.example.exams.mapper;
 
 import com.example.exams.dtos.TeacherDto;
 import com.example.exams.entity.Teachers;
+import org.modelmapper.ModelMapper;
 
 public class TeacherMapper {
+    private static ModelMapper modelMapper = new ModelMapper();
+
+    public TeacherMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
     //map dto to entity
     public static Teachers mapToteacher(TeacherDto teacherDto){
 
-        Teachers teachers=new Teachers(
-                teacherDto.getId(),
-                teacherDto.getTeacherCode(),
-                teacherDto.getTeacherName(),
-                teacherDto.getTphone()
-
-
-        );
+        Teachers teachers=new Teachers();
+        teachers=modelMapper.map(teacherDto,Teachers.class);
         return teachers;
     }
     //map entity to dto
     public static TeacherDto mapToTeacherDto(Teachers teachers){
-        TeacherDto teacherDto=new TeacherDto(
-                teachers.getId(),
-                teachers.getTeacherCode(),
-                teachers.getTeacherName(),
-                teachers.getTphone()
-
-
-        );
+        TeacherDto teacherDto=new TeacherDto();
+        teacherDto=modelMapper.map(teachers,TeacherDto.class);
         return teacherDto;
     }
 }

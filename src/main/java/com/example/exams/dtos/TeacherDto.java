7bbf28c7cf8d6937763d5cb9f1320f63ce561a0a
keneth217 <kipyegonkeneth03@gmail.com>
2024-03-02 11:43:25 +0300

    teacher dto mapper

--- src/main/java/com/example/exams/dtos/TeacherDto.java
+++ src/main/java/com/example/exams/dtos/TeacherDto.java
@@ -2,9 +2,11 @@ package com.example.exams.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TeacherDto {
     private Long id;
     private String teacherCode;

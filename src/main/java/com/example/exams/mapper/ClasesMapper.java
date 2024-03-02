9918dc409f7d075964b82db1086078fb55fca752
keneth217 <kipyegonkeneth03@gmail.com>
2024-03-02 11:41:41 +0300

    Teacher mapper

--- src/main/java/com/example/exams/mapper/ClasesMapper.java
+++ src/main/java/com/example/exams/mapper/ClasesMapper.java
@@ -1,38 +1,33 @@
 package com.example.exams.mapper;
 
+import com.example.exams.dtos.ClassDto;
 import com.example.exams.dtos.StudentsDto;
 import com.example.exams.dtos.SubjectDto;
+import com.example.exams.entity.Classes;
 import com.example.exams.entity.Students;
 import com.example.exams.entity.Subjects;
+import org.modelmapper.ModelMapper;
+
 import java.util.stream.Collectors;
 
 public class ClasesMapper {
-    //map dto to entity
-    public static Subjects mapToSubject(SubjectDto subjectDto){
+    private static ModelMapper modelMapper  =new ModelMapper();;
 
-        Subjects subjects=new Subjects(
-                subjectDto.getId(),
-                subjectDto.getSubjectAlias(),
-                subjectDto.getSubjectName(),
-                subjectDto.getSubjectCategory(),
-                subjectDto.getOutOf(),
-                subjectDto.getTotalScore()
-        );
-        return subjects;
+    public ClasesMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    //map dto to entity
+    public static Classes mapToClasses(ClassDto classDto){
+
+        Classes class1= new Classes();
+        class1=modelMapper.map(classDto,Classes.class);
+        return class1;
     }
     //map entity to dto
-    public static SubjectDto mapToSubjectsDtos(Subjects subjects){
-        SubjectDto subjectDto = new SubjectDto(
-                subjects.getId(),
-                subjects.getSubjectAlias(),
-                subjects.getSubjectName(),
-                subjects.getSubjectCategory(),
-                subjects.getSubjectCode(),
-                subjects.getOutOf(),
-                subjects.getTotalScore()
-
-
-        );
-        return subjectDto;
+    public static ClassDto mapToSubjectsDtos(Classes classes){
+       ClassDto classDto=new ClassDto();
+       classDto=modelMapper.map(classes,ClassDto.class);
+        return classDto;
     }
 }

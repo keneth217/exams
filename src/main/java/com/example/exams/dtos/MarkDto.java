74a729aec72d35ef3e399fc4f7c595c0c854633b
keneth217 <kipyegonkeneth03@gmail.com>
2024-03-04 14:40:59 +0300

    mark dto

---
+++ src/main/java/com/example/exams/dtos/MarkDto.java
@@ -0,0 +1,22 @@
+package com.example.exams.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MarkDto {
+    private Long markId;
+    private String admNo;
+    private String  subjectCode;
+    private String teacherCode;
+    private String studentClass;
+    private String totalMarks;
+    private String actualScore;
+    private String grade;
+    private String termName;
+}

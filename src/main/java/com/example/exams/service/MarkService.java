f2562f44523ef7af95a8ad09daa697789fd6fc98
keneth217 <kipyegonkeneth03@gmail.com>
2024-03-04 14:42:02 +0300

    mark service

---
+++ src/main/java/com/example/exams/service/MarkService.java
@@ -0,0 +1,14 @@
+package com.example.exams.service;
+
+import com.example.exams.dtos.MarkDto;
+import com.example.exams.dtos.StudentsDto;
+import com.example.exams.repository.MarksRepository;
+
+import java.time.LocalDateTime;
+
+public interface MarkService {
+    MarkDto  enterMarks(MarkDto markDto ,String grade, String createdBy, LocalDateTime createdAt);
+  MarkDto updateMark(String admNo,String subjectCode,String studentClass,String teacherCode,String termName ,MarkDto updateMark);
+
+
+}

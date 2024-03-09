6d076488a89349b0ae3ae2b9f72ae6256846ee8d
keneth217 <kipyegonkeneth03@gmail.com>
2024-03-09 15:41:20 +0300

    exams api

--- src/main/java/com/example/exams/service/TeacherService.java
+++ src/main/java/com/example/exams/service/TeacherService.java
@@ -1,10 +1,17 @@
 package com.example.exams.service;
 
-import com.example.exams.dtos.StudentsDto;
+import com.example.exams.dtos.ClassDto;
+import com.example.exams.dtos.SubjectDto;
 import com.example.exams.dtos.TeacherDto;
 
 import java.time.LocalDateTime;
 
 public interface TeacherService {
     TeacherDto addTeacher(TeacherDto teacherDto,String createdBy, LocalDateTime createdAt);
+
+//    SubjectDto assignSubjects(Long teacherId, Long subjectId, TeacherDto teacherDto, SubjectDto subjectDto);
+
+    TeacherDto assignSubjectToTeacher(Long teacherId, Long subjectId, Long classId);
+
+    TeacherDto getTeacher(Long id);
 }

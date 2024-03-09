8523f7172c6d0c0d6d4db447043b054a23837e05
keneth217 <kipyegonkeneth03@gmail.com>
2024-03-09 15:39:38 +0300

    exams api

--- src/main/java/com/example/exams/dtos/SubjectDto.java
+++ src/main/java/com/example/exams/dtos/SubjectDto.java
@@ -1,7 +1,10 @@
 package com.example.exams.dtos;
 
 
+import com.example.exams.entity.Classes;
 import com.example.exams.entity.Students;
+import com.example.exams.entity.Teachers;
+import jakarta.persistence.Column;
 import lombok.*;
 
 import java.util.Set;
@@ -11,13 +14,13 @@ import java.util.Set;
 @NoArgsConstructor
 public class SubjectDto {
     private Long id;
+    @Column(name = "subject_code")
     private int subjectCode;
     private String subjectName;
     private String  subjectCategory;
     private String subjectAlias;
-    private int totalScore;
-    private int outOf;
-    private Set<Students> student;
-
-//    private Set<Students> students;
+//    private int totalScore;
+//    private int outOf;
+//    private Set<Teachers> teachers;
+    private Set<Classes> classes;
 }

3fcd0fcc57623fa7c4a3f9371fa9d6c11dec9aac
keneth217 <kipyegonkeneth03@gmail.com>
2024-03-09 15:38:38 +0300

    exams api

--- src/main/java/com/example/exams/dtos/MarkDto.java
+++ src/main/java/com/example/exams/dtos/MarkDto.java
@@ -1,5 +1,6 @@
 package com.example.exams.dtos;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,11 +13,13 @@ import lombok.Setter;
 public class MarkDto {
     private Long markId;
     private String admNo;
-    private String  subjectCode;
-    private String teacherCode;
+    private String subjectCode;
     private String studentClass;
     private String totalMarks;
     private String actualScore;
     private String grade;
     private String termName;
+    private String studentAdmNo;
+    private String teacherCode;
+
 }

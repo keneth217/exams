565ffef2fb0d817f587d5f9dde16c07dc2889c4d
keneth217 <kipyegonkeneth03@gmail.com>
2024-03-09 15:41:09 +0300

    exams api

--- src/main/java/com/example/exams/dtos/TeacherDto.java
+++ src/main/java/com/example/exams/dtos/TeacherDto.java
@@ -1,15 +1,19 @@
 package com.example.exams.dtos;
 
+import com.example.exams.entity.Subjects;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class TeacherDto {
     private Long id;
-    private String teacherCode;
-    private String teacherName;
+    private String tCode;
+    private String tName;
     private String tphone;
+    private Set<Subjects> subjects;
 }

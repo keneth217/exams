356771444034e495bf4a71cefe437afbc90e9a25
keneth217 <kipyegonkeneth03@gmail.com>
2024-03-09 15:40:05 +0300

    exams api

--- src/main/java/com/example/exams/entity/Teachers.java
+++ src/main/java/com/example/exams/entity/Teachers.java
@@ -1,13 +1,13 @@
 package com.example.exams.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Setter
 @Getter
@@ -18,7 +18,15 @@ public class Teachers  extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String teacherCode;
-    private String teacherName;
+    private String tCode;
+    private String tName;
     private String tphone;
+
+    @ManyToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    @JoinTable(name="teacher_subjects",
+            joinColumns =   @JoinColumn(name="teacher_id",referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name="subject_id",referencedColumnName = "id")
+    )
+    private Set<Subjects> subjects = new HashSet<>();
+
 }

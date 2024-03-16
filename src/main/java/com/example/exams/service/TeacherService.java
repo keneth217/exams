932faae518dc0ce6b006f5a55ce77d1c797d295e
keneth217 <kipyegonkeneth03@gmail.com>
2024-03-16 14:32:38 +0300

    teachers controller

--- src/main/java/com/example/exams/service/TeacherService.java
+++ src/main/java/com/example/exams/service/TeacherService.java
@@ -5,13 +5,20 @@ import com.example.exams.dtos.SubjectDto;
 import com.example.exams.dtos.TeacherDto;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 public interface TeacherService {
     TeacherDto addTeacher(TeacherDto teacherDto,String createdBy, LocalDateTime createdAt);
 
 //    SubjectDto assignSubjects(Long teacherId, Long subjectId, TeacherDto teacherDto, SubjectDto subjectDto);
 
-    TeacherDto assignSubjectToTeacher(Long teacherId, Long subjectId, Long classId);
+//    TeacherDto assignSubjectToTeacher(Long teacherId, Long subjectId, Long classId);
+
+
+    //    A teacher can be associated with multiple subjects through a many-to-many relationship.
+//    A subject can belong to only one class through a many-to-one relationship.
+//    A class can have multiple subjects through a one-to-many relationship.
+    TeacherDto assignSubjectToTeacher(Long teacherId, Long subjectId, Set<Long> classIds);
 
     TeacherDto getTeacher(Long id);
 }
